23112506538af63c3f716be08d69171e7cc61433
Thanhp <thanhp@vn.innova.com>
2020-10-10 15:33:54 +0700

    Update get information

--- spiders/Collect_vin.py
+++ spiders/Collect_vin.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.selector import Selector
 import csv
+import os
 class VinSpider(scrapy.Spider):
     name='input_vin'
     list_result=[]
@@ -31,13 +32,19 @@ class VinSpider(scrapy.Spider):
     start_urls=['https://vpic.nhtsa.dot.gov/decoder']
 
     def parse(self,response):
+        root=os.getcwd()
+        Pathfile='input_VIN.txt'
+        Chuoi_Vin=[]
+        with open(Pathfile,'r') as f:
+            Chuoi_Vin.append(f.readline())
         token = response.css('input[name="__RequestVerificationToken"]::attr(value)').extract_first()
-        data={
-            '__RequestVerificationToken':token,
-            'VIN':'2HNYD28858H540765',
-            'ModelYear':''        
-        }        
-        yield scrapy.FormRequest(url=self.url_vin,formdata=data,callback=self.parse_Vin)
+        for a in Chuoi_Vin:                   
+            data={
+                '__RequestVerificationToken':token,
+                'VIN':a,
+                'ModelYear':''        
+            }        
+            yield scrapy.FormRequest(url=self.url_vin,formdata=data,callback=self.parse_Vin)
     def parse_Vin(self,response):
         hsx=Selector(response)  
         vehicle={}      
@@ -48,20 +55,20 @@ class VinSpider(scrapy.Spider):
         vehicle['Make']=hsx.xpath("//span[@id='decodedMake']/text()").get()
         vehicle['Model']=hsx.xpath("//span[@id='decodedModel']/text()").get()
         vehicle['Bodyclass']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[7]/text()").get()
-        vehicle['Series']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divOtherDetails']/div[1]/div[2]/div[1]/div[1]/text()").get()
-        vehicle['Cylinder']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divOtherDetails']/div[1]/div[2]/div[3]/div[1]/text()").get()
-        vehicle['Trim']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divOtherDetails']/div[1]/div[2]/div[1]/div[2]/text()").get()
-        vehicle['EngineModel']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divOtherDetails']/div[1]/div[2]/div[5]/div[1]/text()").get()
-        vehicle['EngineDisplacement']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divOtherDetails']/div[1]/div[2]/div[7]/div[2]/text()").get()
-        #fields=['VIN','Man','VehType','Year','Make','Model','Bodyclass','Series','Cylinder','Trim','EngineModel','EngineDisplacement']
+        vehicle['Series']=hsx.xpath('//label[contains(text(),"Series:")]/following-sibling::text()').get()
+        vehicle['Cylinder']=hsx.xpath("//label[contains(text(),'Cylinders:')]/following-sibling::text()").get()
+        vehicle['Trim']=hsx.xpath("//label[contains(text(),'Trim:')]/following-sibling::text()").get()
+        vehicle['EngineModel']=hsx.xpath("//label[contains(text(),'Engine Model:')]/following-sibling::text()").get()
+        vehicle['FuelType']=hsx.xpath("//label[contains(text(),' Primary Fuel Type:')]/following-sibling::text()").get()
+        vehicle['EngineDisplacement']=hsx.xpath("//label[contains(text(),' Engine Displacement (L):')]/following-sibling::text()").get()
         print(vehicle['Trim'])
         self.list_result.append(vehicle)        
         filename = 'result.csv'        
         with open(filename, 'w',newline="") as f:
-            writer=csv.DictWriter(f,['Vin','Man','VehType','Year','Make','Model','Bodyclass','Series','Cylinder','Trim','EngineModel','EngineDisplacement'])
-            writer.writeheader()
-            for data in self.list_result:
-                writer.writerow(data)            
+            writer=csv.DictWriter(f,['Vin','Man','VehType','Year','Make','Model','Bodyclass','Series','Cylinder','Trim','EngineModel','FuelType','EngineDisplacement'])
+            writer.writeheader()            
+            writer.writerow(vehicle)
+        yield parse()          
 def run_Vin(VinSpider):      
     process=CrawlerProcess()
     process.crawl(VinSpider)
