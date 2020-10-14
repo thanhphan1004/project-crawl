f62ad58f1c377363a53290c3b24168a7642da5ff
Thanhp <thanhp@vn.innova.com>
2020-10-14 16:44:16 +0700

    Update code

--- spiders/UI.py
+++ spiders/UI.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from tkinter import *
 from Collect_vin import *
 from tkinter.filedialog import *
@@ -10,10 +11,15 @@ lbl=Label(window,text="Hello",font=("Arial Bold",50))
 lbl.grid(column=0,row=0)
 def clicked():
     feedback= askopenfilename()
-    pathOld=feedback.find
-    os.rename=()
-    print(type(feedback))
-    VinSpider.run_Vin(VinSpider)
+    pathOld=feedback.rfind("/")
+    path=feedback[0:(int(pathOld)+1)]
+    Oldname=feedback[(int(pathOld)+1):int(len(feedback))]    
+    os.listdir(path)
+    newName="input_VIN.txt"
+    os.rename=(path+Oldname,path+newName)
+    print(feedback)
+    print(newName)
+    #VinSpider.run_Vin(VinSpider)
 btn=Button(window,text="NHTSA Decode",height=5,width=20,command=clicked)
 btn.place(x=200,y=300)
 
