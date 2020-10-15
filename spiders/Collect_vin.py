8b967aee65767847d9c26ffd5c72002aee5461c4
Thanhp <thanhp@vn.innova.com>
2020-10-15 16:32:14 +0700

    Update code lates

--- spiders/Collect_vin.py
+++ spiders/Collect_vin.py
@@ -1,9 +1,12 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerProcess,CrawlerRunner
 from scrapy.selector import Selector
 from scrapy.exceptions import CloseSpider
 import csv
 import os
+from multiprocessing.context import Process
+from scrapy.exceptions import CloseSpider
+from twisted.internet import protocol, reactor, endpoints
 class VinSpider(scrapy.Spider):
     name='input_vin'
     list_result=[]
@@ -33,7 +36,6 @@ class VinSpider(scrapy.Spider):
     ListVin=[]
     start_urls=['https://vpic.nhtsa.dot.gov/decoder']
     def parse(self,response):
-        #root=os.getcwd()
         token = response.css('input[name="__RequestVerificationToken"]::attr(value)').extract_first()
         Pathfile='input_VIN.txt'   
         with open(Pathfile,'r') as f:
@@ -56,34 +58,28 @@ class VinSpider(scrapy.Spider):
                 yield scrapy.FormRequest(url=self.url_vin,formdata=data,callback=self.parse_Vin)              
     def parse_Vin(self,response):
         hsx=Selector(response)
-        aa=response.body
-        '''filename = 'result.txt'
-        with open(filename, 'w') as f:
-            f.write(response.text)'''
+        root=os.getcwd()
         vehicle={'Vin':' ','Man':' ','VehType':' ','Year':' ','Make':' ','Model':' ','Bodyclass':' ','Series':' ','Cylinder':' ','Trim':' ','EngineModel':' ','FuelType':' ','EngineDisplacement':' '}      
-        vehicle['Vin']=hsx.xpath("//input[@id='VIN']").attrib['value'].strip()
-        vehicle['Man']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[1]/a[1]/text()").get().strip()
-        vehicle['VehType']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[3]/text()").get().strip()
-        vehicle['Year']=hsx.xpath("//span[@id='decodedModelYear']/text()").get().strip()
-        vehicle['Make']=hsx.xpath("//span[@id='decodedMake']/text()").get().strip()
-        vehicle['Model']=hsx.xpath("//span[@id='decodedModel']/text()").get().strip()
-        vehicle['Bodyclass']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[7]/text()").get().strip()  
+        vehicle['Vin']=hsx.xpath("//input[@id='VIN']").attrib['value']
+        vehicle['Man']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[1]/a[1]/text()").get()
+        vehicle['VehType']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[3]/text()").get()
+        vehicle['Year']=hsx.xpath("//span[@id='decodedModelYear']/text()").get()
+        vehicle['Make']=hsx.xpath("//span[@id='decodedMake']/text()").get()
+        vehicle['Model']=hsx.xpath("//span[@id='decodedModel']/text()").get()
+        vehicle['Bodyclass']=hsx.xpath("//body/div[@id='outerDiv']/div[@id='divCommonDetails']/div[@id='searchform']/div[1]/div[2]/div[2]/div[1]/p[7]/text()").get()
         Series=hsx.xpath('//label[contains(text(),"Series:")]/following-sibling::text()').get()
-        #vehicle['Series']=hsx.xpath('//label[contains(text(),"Series:")]/following-sibling::text()').get()
         if((Series is None)==False):
             vehicle['Series']=Series.strip()
         vehicle['Cylinder']=hsx.xpath("//label[contains(text(),'Cylinders:')]/following-sibling::text()").get().strip()
         Trim=hsx.xpath("//label[contains(text(),'Trim:')]/following-sibling::text()").get()
-        #vehicle['Trim']=hsx.xpath("//label[contains(text(),'Trim:')]/following-sibling::text()").get().strip()
         if((Trim is None)==False):
             vehicle['Trim']=Trim.strip()
         vehicle['EngineModel']=hsx.xpath("//label[contains(text(),'Engine Model:')]/following-sibling::text()").get().strip()
         vehicle['FuelType']=hsx.xpath("//label[contains(text(),' Primary Fuel Type:')]/following-sibling::text()").get().strip()
         vehicle['EngineDisplacement']=hsx.xpath("//label[contains(text(),' Engine Displacement (L):')]/following-sibling::text()").get().strip()
         self.list_result.append(vehicle)
-        print(len(self.list_result))
         if(len(self.list_result) == len(self.ListVin)):
-            filename = 'result.csv'        
+            filename = root+'\\result.csv'        
             with open(filename, 'w',newline="") as f:
                 writer=csv.DictWriter(f,['Vin','Man','VehType','Year','Make','Model','Bodyclass','Series','Cylinder','Trim','EngineModel','FuelType','EngineDisplacement'])
                 writer.writeheader()
@@ -93,4 +89,4 @@ class VinSpider(scrapy.Spider):
         process=CrawlerProcess()
         process.crawl(VinSpider)
         process.start()
-#VinSpider.run_Vin(VinSpider)
+VinSpider.run_Vin(VinSpider)
--- spiders/UI.py
+++ spiders/UI.py
@@ -1,14 +1,19 @@
-import os
 import shutil
 from tkinter import *
 from Collect_vin import *
 from tkinter.filedialog import *
+from multiprocessing.context import Process
+import time
 root=os.getcwd()
 window = Tk()
 window.title("VIN's Aggregator")
 window.geometry('1000x400')
 lbl=Label(window,text="Hello",font=("Arial Bold",50))
 lbl.grid(column=0,row=0)
+lbl_result=Label(window,text="",font=("Arial Bold",20))
+lbl_result.place(x=200,y=250)
+lbl_note=Label(window,text="",font=("Arial Bold",10))
+lbl_note.place(x=200,y=300)
 def clicked():
     feedback= askopenfilename()
     data=[]
@@ -24,10 +29,12 @@ def clicked():
             i=i+1
             if(i<len(chuoi)):
                 f.write('\n')
-    f.close()
+    f.close()    
     VinSpider.run_Vin(VinSpider)
+    time.sleep(1)
+    lbl_result.configure(text="crawl success")
+    lbl_note.configure(text="Vui lòng khởi động lại Soft")
 btn=Button(window,text="NHTSA Decode",height=5,width=20,command=clicked)
-btn.place(x=200,y=300)
-
+btn.place(x=200,y=150)
 window.mainloop()
 
